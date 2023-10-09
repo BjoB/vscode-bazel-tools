68ed52fb11a93600907daa201501c2ffe1f44aa6
Bat <bjoern.barschtipan@gmail.com>
2023-10-09 11:54:38 +0200

    Push logs to output channel

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,18 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as api from './api';
+import {logger} from './logging';
 
+let extensionOutputChannel : vscode.OutputChannel | undefined;
 let compileCommandsGenerator: vscode.Disposable | undefined;
 
 export async function activate(context: vscode.ExtensionContext) {
+	extensionOutputChannel = vscode.window.createOutputChannel("vsc-bazel-tools");
+	extensionOutputChannel.show();
+	logger.attachTransport((logObj) => { 
+		extensionOutputChannel.appendLine(logObj['0'].toString());
+	});
+
 	compileCommandsGenerator = vscode.commands.registerCommand('vsc-bazel-tools.generateCompileCommands', async () => {
 		let currentlyOpenTabFileDir = path.dirname(vscode.window.activeTextEditor?.document.uri.fsPath!);
 		let customCompileCommandsTarget = vscode.workspace.getConfiguration("vsc-bazel-tools");
