7387b20745c94cbf45f8cfdc33ce5dc3fa401362
Bat <bjoern.barschtipan@gmail.com>
2023-12-13 16:33:20 +0100

    Reduce timeout for success msg

--- src/extension.ts
+++ src/extension.ts
@@ -60,7 +60,7 @@ export async function activate(context: vscode.ExtensionContext) {
 			return new Promise<void>(resolve => {
 				setTimeout(() => {
 					resolve();
-				}, 3000);
+				}, 1000);
 			});
 		});
 	});
